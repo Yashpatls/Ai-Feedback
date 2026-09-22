b38ffa9ee03c9155420fdb23a08b1acf7e36e5ed
yash Varmora <yashpatel1042@gmail.com>
2026-09-22 10:11:39 +0530

    fix: forcefully override NEXTAUTH_URL on Render

--- app/patch-env.ts
+++ app/patch-env.ts
@@ -1,5 +1,5 @@
 if (typeof process !== "undefined") {
-  if (process.env.RENDER_EXTERNAL_URL && !process.env.NEXTAUTH_URL) {
+  if (process.env.RENDER_EXTERNAL_URL) {
     process.env.NEXTAUTH_URL = process.env.RENDER_EXTERNAL_URL;
   } else if (process.env.NEXTAUTH_URL === "") {
     delete process.env.NEXTAUTH_URL;
